4411f65cc566039f5eb7be09842d4dbce3b19941
Prakhar <techloverpandeyprakhar@gmail.com>
2022-02-17 15:00:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let allbtn = document.querySelectorAll('a');
+let audio = document.getElementById('click_sound')
+
+for (const items of allbtn) {
+    items.addEventListener('click',()=>{
+        audio.play();
+    })
+}
